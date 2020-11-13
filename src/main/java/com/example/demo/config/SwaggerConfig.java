1b2154d6b5ffff646750da7cc4b795e5430d68f9
chenxiang <328692993@qq.com>
2020-11-13 12:11:33 +0800

    swagger添加header

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -6,28 +6,37 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashSet;
+import java.util.List;
+
+import static com.example.demo.context.ServiceContext.TOKEN_HEADER;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private List<ApiKey> securitySchemes() {
+        return Lists.newArrayList(new ApiKey(TOKEN_HEADER, TOKEN_HEADER, "header"));
+    }
+
     @Bean
-    public Docket documentation(){
+    public Docket documentation() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
                 .build()
                 .protocols(new HashSet<String>(Lists.newArrayList("http")))
                 .pathMapping("/")
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .securitySchemes(securitySchemes());
     }
 
-    private ApiInfo apiInfo(){
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("服务API")
                 .description("服务端后台接口说明文档")
--- src/main/java/com/example/demo/util/JWTUtils.java
+++ src/main/java/com/example/demo/util/JWTUtils.java
@@ -122,4 +122,8 @@ public class JWTUtils {
         UserToken userToken = UserToken.builder().id(getUserId(token)).name(getLoginName(token)).build();
         ThreadLocalContextAccessor.setServiceContext(new ServiceContext(userToken));
     }
+
+    public static void main(String[] args) {
+        log.info(createToken("0210dd81c50b35b0da0bc4144186a5d7","test",null, LocalDateTime.now().plusYears(1L), signatureAlgorithm256));
+    }
 }
