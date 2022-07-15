361c054926c8ccc2a0024d60f6d138c5cf828516
chenxiang <328692993@qq.com>
2022-07-15 19:23:23 +0800

    跨域问题解决

--- src/main/java/com/example/demo/beanfactory/ServiceBeanFactory.java
+++ src/main/java/com/example/demo/beanfactory/ServiceBeanFactory.java
@@ -14,6 +14,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -176,6 +179,16 @@ public class ServiceBeanFactory implements WebMvcConfigurer {
         ThreadLocalContextAccessor.setServiceContext(sc);
     }
 
-
+    @Bean
+    public CorsFilter corsFilter()
+    {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("http://localhost:8080");
+        corsConfiguration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
 
 }
